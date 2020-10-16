efe225ec08fc2cccaf7594aa8ff5778b416dea39
Pepe Millan Fernandez <jose.millan@iesdonana.org>
2020-10-16 11:31:41 +0200

    Tarea terminada

--- tareas/tarea1/src/app/warning/warningalert.component.ts
+++ tareas/tarea1/src/app/warning/warningalert.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: '.warning',
-    template: '<script>alert("warning")</script>'
+    template: `<div class="alert alert-warning" role="alert" onclick="alert('warning')">Esto es un warning</div>`,
+    styleUrls: ['./warningalert.component.css']
 })
 export class WarningComponent {
 
