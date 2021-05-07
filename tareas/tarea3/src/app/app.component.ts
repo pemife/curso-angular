d2f35fd0f3ce637072ca26777761cd8fd3429134
Pepe Millan Fernandez <jose.millan@iesdonana.org>
2021-05-07 17:39:20 +0200

    Tarea3 a puntito caramelo

--- tareas/tarea3/src/app/app.component.ts
+++ tareas/tarea3/src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { withLatestFrom } from 'rxjs-compat/operator/withLatestFrom';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: [`
+      .fondo {
+        color: white;
+      }
+  `]
 })
 export class AppComponent {
   display = false;
@@ -11,7 +17,17 @@ export class AppComponent {
   nClicks = 1;
 
   onButtonPress() {
-    this.display=!this.display
-    this.clicks.push('Click - 1 - ' + new Date() )
+    this.display=!this.display;z
+    this.clicks.push([ this.nClicks, 'Click - ' + this.nClicks + ' - ' + new Date()]);
+    this.nClicks++;
+    // console.log(this.clicks);
+  }
+
+  getColor(numero) {
+    let color: Array<String>;
+    color['fondo'] = numero > 5 ? 'blue' : 'white';
+    color['letra'] = numero > 5 ? 'white' : 'black';
+    console.log(color);
+    return color;
   }
 }
