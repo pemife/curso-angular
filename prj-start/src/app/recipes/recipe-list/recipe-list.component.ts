2e00171be738d7a42a65cd568aa576ba9f37c4b5
Pepe Millan Fernandez <jose.millan@iesdonana.org>
2021-05-19 12:57:38 +0200

    Mitad clase 55

--- prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
+++ prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://c.pxhere.com/photos/da/85/Diet_Foods_Salad_Salads_Tomato_Tomatoes-1614557.jpg!d')
+  ];
 
   constructor() { }
 
