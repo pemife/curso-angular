87bd3e8f76cd8deeeaa6d22dd46fd1cafaf31ed9
José Millán Fernández <pepemillanfernandez@gmail.com>
2021-06-06 18:14:55 +0200

    Clase 78

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,17 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { 
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ComponentFactoryResolver,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +19,51 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   //encapsulation: ViewEncapsulation.None             // Atributos inpuestos automáticamente por angular (deshabilitados) Native -> ShadowDom (buscar)
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+  OnInit,
+  OnChanges,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+  {
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('Constructor called!');
   }
 
+  ngOnInit(): void {
+    console.log('ngOnInit called!');
+  }
+
+  ngOnChanges( changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit() {
+    console.log('AfterContentInit called!');
+  }
+
+  ngAfterContentChecked() {
+    console.log('AfterContentChecked called!');
+  }
+  ngAfterViewInit() {
+    console.log('AfterViewInit called!');
+  }
+
+  ngAfterViewChecked() {
+    console.log('AfterViewChecked called!');
+  }
+
+  ngOnDestroy() {
+    console.log('OnDestroy called!');
+  }
 }
